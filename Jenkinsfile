410e9a722c13e2af3f16095c36e658c4f946b976
Xenon <lgasyou@163.com>
2020-06-19 11:35:32 +0800

    ci(jenkins): 增加判断是否需要构建的逻辑判断，删除Jenkinsfile中的测试阶段

--- Jenkinsfile
+++ Jenkinsfile
@@ -12,16 +12,6 @@ pipeline {
                 sh './jenkins/scripts/build.sh' 
             }
         }
-        stage('Test') {
-            steps {
-                sh 'mvn test' 
-            }
-            post {
-                always {
-                    junit "target/surefire-reports/*.xml"
-                }
-            }
-        }
         stage('Deliver') {
             steps {
                 sh './jenkins/scripts/deliver.sh' 
--- jenkins/scripts/common.sh
+++ jenkins/scripts/common.sh
@@ -20,6 +20,7 @@ readonly IMAGE_PREFIX="lgasyou"
 
 tag=""
 full_image_name=""
+newest_image_existed=""
 
 function build::package-and-build-image() {
   mvn "${MVN_OPTS}" clean package spring-boot:repackage dockerfile:build
@@ -33,9 +34,21 @@ function build::build-all-image() {
   build::install-modules
 
   for module in ${SUBMODULES[*]}; do
-    echo "Building image of module ${module}..."
     cd "${module}"
-    build::package-and-build-image
+
+    # Try to pull image first
+    util::get-project-version
+    full_image_name="${IMAGE_PREFIX}/${module}:${tag}"
+    util::try-pull-image
+
+    # Only if image not exists we start building it.
+    util::has-newest-image-existed "${IMAGE_PREFIX}/${module}"
+    if [[ -z ${newest_image_existed} ]]; then
+      echo "Building image of module ${module}..."
+      build::package-and-build-image
+    else
+      echo "Skipped module ${module}:${tag} because it has already been built."
+    fi
     cd "${BASE_DIR}"
   done
 }
@@ -53,6 +66,20 @@ function deliver::deliver-all() {
     deliver::deliver
     cd "${BASE_DIR}"
   done
+}
+
+function util::try-pull-image() {
+  docker pull "${full_image_name}" || true
+}
+
+function util::has-newest-image-existed() {
+  local repository=$1
+  image_exists=$(docker images "${repository}" | awk '{print $2}')
+  if [[ ${image_exists} == 'TAG' ]]; then
+    newest_image_existed=""
+  else
+    newest_image_existed=$(echo "${image_exists}" | grep -v 'TAG' | grep "${tag}")
+  fi
 }
 
 function util::get-project-version() {
